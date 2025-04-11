adcc9bbfc481aa96a1e32c9c672e8cc06a6ac387
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-11 15:43:18 +0300

    window object

---
+++ JavaScript/Window Object/window.js
@@ -0,0 +1,12 @@
+var val = window;
+
+
+val = window.location.host;
+val = window.location;
+val = window.scrollX;
+val = window.scrollY
+
+
+//window.location.href = 'https://www.google.com.tr/?hl=tr';
+
+console.log(val); 
